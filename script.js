76b7ab458d8aefb63466d86b8bd3be1f0b80e5a4
Bobi-DA <masterjb81@gmail.com>
2024-06-24 17:54:00 +0200

    name + id 2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let namePokemon = [];
-const BASE_URL = "https://pokeapi.co/api/v2/";
-const OFFSET = "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0";
+let picturePokemon = [];
+
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/1";
+const OFFSET = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
 
 
 
@@ -12,39 +14,6 @@ function init() {
     fetchDataJsonBase();
 }
 
-async function fetchDataJsonOffset() {
-    let response = await fetch(OFFSET);         // Holt die Daten von der API
-    let responseAsJson = await response.json(); // Wandelt die Antwort in ein JSON-Objekt um.
-    let results = responseAsJson['results'];   // Speichert das Ergebnis in der Variable - namePoke = (5) [{…}, {…}, {…}, {…}, {…}]
-
-
-    for (let i = 0; i < results.length; i++) {
-        const result = results[i];
-
-        console.log(result.name);
-        namePokemon.push(result.name);
-
-        cardPokemon(i);
-    }
-}
-
-async function fetchDataJsonBase() {
-    let response = await fetch(BASE_URL);
-    let responseAsJson2 = await response.json();
-    // let re = responseAsJson['r'];   
-    console.log(responseAsJson2);
-
-    // for (let i = 0; i < re.length; i++) {
-    //     const res = re[i];
-
-    //     console.log(res.name);
-    //     // namePokemon.push(res.name);
-
-    //     // cardPokemon(i);
-    // }
-}
-
-
 
 function render() {
     let content = document.getElementById('content');
@@ -52,7 +21,33 @@ function render() {
 }
 
 
-function cardPokemon(i) {
+async function fetchDataJsonOffset() {
+    let response = await fetch(OFFSET);         // Holt die Daten von der API
+    let responseAsJson = await response.json(); // Wandelt die Antwort in ein JSON-Objekt um.
+    let results = responseAsJson['results'];    // Speichert das Ergebnis in der Variable - namePoke = (5) [{…}, {…}, {…}, {…}, {…}]
+
+
+    for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        namePokemon.push(result.name);
+
+        cardPokemonHTML(i);
+    }
+}
+
+
+async function fetchDataJsonBase() {
+    let response = await fetch(BASE_URL);
+    let responseAsJson2 = await response.json();
+    let re = responseAsJson2;
+
+    console.log(re);
+    picturePokemon.push(re.sprites.other.dream_world.front_default);
+
+}
+
+
+function cardPokemonHTML(i) {
     return content.innerHTML += /*html*/`
 
     <div class="card" style="width: 18rem;">
@@ -60,7 +55,7 @@ function cardPokemon(i) {
             <div>#${i + 1}</div>
             <div>${namePokemon[i]}</div>
         </div>
-        <img src="./img/001.png" class="card-img-top" alt="">
+        <img src="${picturePokemon[i]}" class="card-img-top" alt="">
         <div class="card-body">
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                 card's content.</p>
