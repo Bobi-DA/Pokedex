7cf02d12b4ee2d41f079c4f75ba24e181b989c3c
Bobi-DA <masterjb81@gmail.com>
2024-06-26 12:50:21 +0200

    name + id + picture

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let namePokemon = [];
-let picturePokemon = [];
-
+let urlPokemon = [];
+let picturesPokemon = [];
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/1";
 const OFFSET = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
 
@@ -11,7 +11,6 @@ const OFFSET = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
 function init() {
     render();
     fetchDataJsonOffset();
-    fetchDataJsonBase();
 }
 
 
@@ -26,28 +25,28 @@ async function fetchDataJsonOffset() {
     let responseAsJson = await response.json(); // Wandelt die Antwort in ein JSON-Objekt um.
     let results = responseAsJson['results'];    // Speichert das Ergebnis in der Variable - namePoke = (5) [{…}, {…}, {…}, {…}, {…}]
 
-
     for (let i = 0; i < results.length; i++) {
         const result = results[i];
         namePokemon.push(result.name);
-
-        cardPokemonHTML(i);
+        urlPokemon.push(result.url);
+        
+        showPicturePokemon(i);
     }
 }
 
+async function showPicturePokemon(i) {
+    let response = await fetch(urlPokemon[i]);       // Holt die Daten von der API
+    let responseAsJson2 = await response.json(); // Wandelt die Antwort in ein JSON-Objekt um.
+    let picturePokemonUrl = responseAsJson2.sprites.other.dream_world.front_default;
 
-async function fetchDataJsonBase() {
-    let response = await fetch(BASE_URL);
-    let responseAsJson2 = await response.json();
-    let re = responseAsJson2;
+    console.log('Picture: ', picturePokemonUrl);
 
-    console.log(re);
-    picturePokemon.push(re.sprites.other.dream_world.front_default);
-
+    cardPokemonHTML(i, picturePokemonUrl);
 }
 
 
-function cardPokemonHTML(i) {
+
+function cardPokemonHTML(i, picturePokemonUrl) {
     return content.innerHTML += /*html*/`
 
     <div class="card" style="width: 18rem;">
@@ -55,7 +54,7 @@ function cardPokemonHTML(i) {
             <div>#${i + 1}</div>
             <div>${namePokemon[i]}</div>
         </div>
-        <img src="${picturePokemon[i]}" class="card-img-top" alt="">
+        <img src="${picturePokemonUrl}" class="card-img-top m-4" alt="Pokemon-Picture">
         <div class="card-body">
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                 card's content.</p>
